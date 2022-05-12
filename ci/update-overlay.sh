d952c92aff3e53c601f697848b75a28aededdbf7
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-05-12 19:09:58 +0300

    CI: verbose Git operations

--- ci/update-overlay.sh
+++ ci/update-overlay.sh
@@ -49,12 +49,12 @@ push_changes () {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
-  if ! git add -A >/dev/null 2>&1; then
+  if ! git add -A; then
     echo "Failed to add files"
     return 1
   fi
 
-  if git diff-index --quiet HEAD >/dev/null 2>&1; then
+  if git diff-index --quiet HEAD; then
     echo "No changes since the last commit, exiting"
     return 0
   fi
@@ -63,23 +63,23 @@ push_changes () {
 
   message="[skip ci] Automatic update $(date "+%Y-%m-%d") (Build $TRAVIS_BUILD_NUMBER)"
 
-  if ! git commit -m "$message" >/dev/null 2>&1; then
+  if ! git commit -m "$message"; then
     echo "Failed to commit"
     return 1
   fi
 
   echo "Pushing"
 
-  if ! git remote rm origin >/dev/null 2>&1; then
+  if ! git remote rm origin; then
     echo "Failed to remove origin"
     return 1
   fi
 
-  if ! git remote add origin "git@github.com:dimitarnestorov/nix-google-fonts-overlay.git" >/dev/null 2>&1; then
+  if ! git remote add origin "git@github.com:dimitarnestorov/nix-google-fonts-overlay.git"; then
     echo "Failed to add origin";
   fi
 
-  if ! git push origin master --quiet >/dev/null 2>&1; then
+  if ! git push origin master; then
     echo "Failed to push"
     return 1
   fi
